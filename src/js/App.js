1f621e926fbb8780bc7f9f46981be47c727be00a
Valeryia Himzheuskaya <valimisael@yahoo.com>
2021-03-07 17:23:51 +0300

    init: start travel-app task

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/vendor/reset.css';
+import '../css/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Travel App</h1>
+    </div>
+  );
+}
+
+export default App;
