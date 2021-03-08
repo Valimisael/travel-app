bc9cfe6bd2a132cb7f518bc4960f8e66a18c6656
Jimmba <gimvit@gmail.com>
2021-03-08 22:29:06 +0300

    feat: add translations for main page

--- src/js/Classes/Settings.js
+++ src/js/Classes/Settings.js
@@ -1,4 +1,4 @@
-const defaultLanguage = 'en';
+import { defaultLanguage, header } from '../Data/data';
 
 export default class Settings {
     constructor() {
@@ -11,7 +11,14 @@ export default class Settings {
             this.lang = localStorage.getItem('lang')
         } else {
             this.lang = defaultLanguage;
-            localStorage.setItem('lang', this.lang);
+            localStorage.setItem('lang', defaultLanguage)
         }
+    }
+
+    updateLang(lang) {
+        const indexLang = header[this.lang].languages.indexOf(lang);
+        const langList = Object.keys(header);
+        this.lang = langList[indexLang];
+        localStorage.setItem('lang', this.lang);
     }
 }
--- src/js/Components/Header.js
+++ src/js/Components/Header.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import logo from '../../img/logo.png';
+import { header} from '../Data/data';
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.settings = this.props.settings;
+    this.changeLang = this.changeLang.bind(this);
+    this.updateState = this.props.updateState.bind(this);
+  }
+
+  changeLang(e) {
+    this.settings.updateLang(e.target.value);
+    this.updateState(this.settings);
+  }
+  
   render () {
+    const indexLanguage = Object.keys(header).indexOf(this.settings.lang);
+    const activeValue = header[this.settings.lang].languages[indexLanguage];
+    const options = header[this.settings.lang].languages.map((el, index) => {
+      return (
+        <option key = {index} value = {header[this.settings.lang][index]}>{el}</option>
+      )
+    })
+
+    const placeHolder = header[this.settings.lang].placeHolder;
+    
     return (
       <header className="header">
         <a href="/" className="header__item logo">
@@ -12,14 +35,12 @@ export default class Header extends React.Component {
           <div className="header__item search">
             <div className="search__field">
               <label className="icon icon-search" htmlFor="search"></label>
-              <input id="search" type="search" placeholder="Search" />
+              <input id="search" type="search" placeholder={placeHolder} />
             </div>
           </div>
           <div className="header__item localization">
-            <select>
-              <option>English</option>
-              <option>Russian</option>
-              <option>Swedish</option>
+            <select onChange = {this.changeLang} value={activeValue}>
+              {options}
             </select>
           </div>
         </div>
