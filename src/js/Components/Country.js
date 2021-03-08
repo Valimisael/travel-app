b688e7aaa4a351377f7d930245cb5cfae66f5c56
Jimmba <gimvit@gmail.com>
2021-03-09 00:54:39 +0300

    fix: country translations

--- src/js/Components/Country.js
+++ src/js/Components/Country.js
@@ -1,29 +1,34 @@
 import React from 'react';
-import { countries } from '../Data/data';
+import { countries, header } from '../Data/data';
 import Gallery from './Gallery';
 
 export default class Country extends React.Component {
+  constructor(props) {
+    super(props);
+    this.settings = this.props.settings;
+  }
   render () {
     const country = countries[2];
-
+    const lang = this.settings.lang;
+    const data = header[lang];
     return (
       <main>
         <div className="hero">
           <img src={country.src} alt={country.country} />
           <div className="hero__details">
-            <h1>{country.country}</h1>
-            <h2>{country.capital}</h2>
+            <h1>{country.translations[lang].country}</h1>
+            <h2>{country.translations[lang].capital}</h2>
           </div>
         </div>
         <div className="country__wrapper">
           <div className="country__info">
             <div className="country__description">
-              <h2 className="country__title">{`About ${country.country}`}</h2>
-              <div className="country__description-text">{country.description}</div>
+              <h2 className="country__title">{`${data.about}`}</h2>
+              <div className="country__description-text">{country.translations[lang].description}</div>
             </div>
             <div className="country__gallery">
-              <h2 className="country__title">{`${country.country} in photos`}</h2>
-              <Gallery images={country.gallery} />
+              <h2 className="country__title">{`${country.translations[lang].country} ${data.inPhotos}`}</h2>
+              <Gallery images={country.gallery} settings={this.settings}/>
             </div>
           </div>
           <div className="country__widgets"></div>
--- src/js/Data/england.js
+++ src/js/Data/england.js
@@ -9,38 +9,110 @@ const englandGallery = [
   {
     original: bigBen,
     thumbnail: bigBen,
-    thumbnailLabel: 'Big Ben',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    translations: {
+      en: {
+        thumbnailLabel: 'Big Ben',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      ru: {
+        thumbnailLabel: 'Биг Бен',
+        description: 'Лорем ипсум).'
+      },
+      se: {
+        thumbnailLabel: 'Big Ben',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+    }
   },
   {
     original: buckinghamPalace,
     thumbnail: buckinghamPalace,
-    thumbnailLabel: 'Buckingham Palace',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    translations: {
+      en: {
+        thumbnailLabel: 'Buckingham Palace',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      ru: {
+        thumbnailLabel: 'Букингемский дворец',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      se: {
+        thumbnailLabel: 'Buckingham Palace',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+    }
   },
   {
     original: londonEye,
     thumbnail: londonEye,
-    thumbnailLabel: 'London Eye',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    translations: {
+      en: {
+        thumbnailLabel: 'London Eye',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      ru: {
+        thumbnailLabel: 'London Eye',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      se: {
+        thumbnailLabel: 'London Eye',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+    }
   },
   {
     original: tower,
     thumbnail: tower,
-    thumbnailLabel: 'Tower',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    translations: {
+      en: {
+        thumbnailLabel: 'Tower',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      ru: {
+        thumbnailLabel: 'Tower',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      se: {
+        thumbnailLabel: 'Tower',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+    }
   },
   {
     original: towerBridge,
     thumbnail: towerBridge,
-    thumbnailLabel: 'Bridge of Tower',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    translations: {
+      en: {
+        thumbnailLabel: 'Bridge of Tower',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      ru: {
+        thumbnailLabel: 'Bridge of Tower',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      se: {
+        thumbnailLabel: 'Bridge of Tower',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+    }
   },
   {
     original: westminsterAbbey,
     thumbnail: westminsterAbbey,
-    thumbnailLabel: 'Westminster Abbey',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    translations: {
+      en: {
+        thumbnailLabel: 'Westminster Abbey',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      ru: {
+        thumbnailLabel: 'Westminster Abbey',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      se: {
+        thumbnailLabel: 'Westminster Abbey',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+    }
   }
 ];
 
